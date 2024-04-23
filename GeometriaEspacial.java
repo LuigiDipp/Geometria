c1dabc3780981118659a144716feb2c47746e283
Luigi de Azevedo Lopes <108826488+LuigiDipp@users.noreply.github.com>
2024-04-23 10:56:19 -0300

    Atualização

---
+++ GeometriaEspacial.java
@@ -0,0 +1,12 @@
+public class GeometriaEspacial {
+
+    double a;
+    double b;
+    double c; 
+
+
+    GeometriaPlana formula = new GeometriaPlana();
+        public static void main(String[] args) {
+        
+    }
+}
---
+++ GeometriaPlana.java
@@ -0,0 +1,8 @@
+public class GeometriaPlana {
+
+    GeometriaPlana formula = new GeometriaPlana();
+
+    public static main(String[] args) {
+        
+    }
+}
