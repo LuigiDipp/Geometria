ccefbab8d67b4a00b2c0109a309679f7b209019c
Luigi de Azevedo Lopes <108826488+LuigiDipp@users.noreply.github.com>
2024-04-25 11:20:46 -0300

    Calculadora

--- Calculos.java
+++ Calculos.java
@@ -20,15 +20,14 @@ public class Calculos {
     return num2;
     }
 
-    // instancia o objeto da classe (dentre os mesmos nenhum deles tem alguém, a verdade da vida é que todos estamos sozinhos no mundo, ninguém possui ninguém, a ideia de possuir alguém é falha dado a verdade de que nunca conseguiremos controlar-las a nosso favor.)
-    Calculos objeto = new Calculos();
+    // instancia o objeto da classe
+        Calculos objeto = new Calculos();
 
 
     Scanner ler = new Scanner(System.in);
 
-    public static main(String[] args) {
-        
-        System.out.printf("Informe o preço da mercadoria:\n");
-        num1 = ler.nextDouble();
+    public static void main(String[] args) {
+
+        System.out.println(Math.sqrt(25));
     }
 }
